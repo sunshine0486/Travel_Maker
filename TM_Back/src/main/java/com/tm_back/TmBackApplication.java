88267bb14286608ff932857c2dab2b15a2dc68d2
sunshine0486 <sunshine46@naver.com>
2025-09-22 12:07:25 +0900

    Front, Back 베이스

---
+++ TM_Back/src/main/java/com/tm_back/TmBackApplication.java
@@ -0,0 +1,13 @@
+package com.tm_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TmBackApplication.class, args);
+    }
+
+}
---
+++ TM_Back/src/test/java/com/tm_back/TmBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tm_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
